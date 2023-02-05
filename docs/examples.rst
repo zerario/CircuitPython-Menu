73d2b069ecd273d15b148893ced1d38e07a1daff
Zerario <derg@zerario.dev>
2023-02-05 21:15:34 +0100

    Merge with Adafruit Cookiecutter template

---
+++ docs/examples.rst
@@ -0,0 +1,8 @@
+Simple test
+------------
+
+Ensure your device works with this simple test.
+
+.. literalinclude:: ../examples/menu_simpletest.py
+    :caption: examples/menu_simpletest.py
+    :linenos:
--- menu.py
+++ menu.py
@@ -1,3 +1,36 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2023 Zerario <derg@zerario.dev>
+#
+# SPDX-License-Identifier: MIT
+"""
+`menu`
+================================================================================
+
+Interactive CircuitPython menu library using RotaryIO and DisplayIO
+
+
+* Author(s): Zerario <derg@zerario.dev>
+
+Implementation Notes
+--------------------
+
+**Hardware:**
+
+.. todo:: Add links to any specific hardware product page(s), or category page(s).
+  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+
+**Software and Dependencies:**
+
+* Adafruit CircuitPython firmware for the supported boards:
+  https://circuitpython.org/downloads
+
+.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+  based on the library's use of either.
+
+# * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
+# * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
+"""
+
 import displayio
 import terminalio
 import digitalio
@@ -16,6 +49,10 @@ from adafruit_displayio_layout.layouts.grid_layout import GridLayout
 from adafruit_displayio_layout.layouts.page_layout import PageLayout
 
 import utils
+
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/zerario/CircuitPython_menu.git"
 
 
 BLACK = 0x000000
